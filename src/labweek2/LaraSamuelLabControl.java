88fdb29f7c05523589632ccf05f67f8cd7ac868a
Equitelo <samuel_lara@unitec.edu>
2024-07-26 11:43:22 -0600

    El tercero utilice lo que se investigo en este laboratorio, el cual era un subtring. En este caso como estructura condicional use el switch, en vez de el if. Dentro de este si usamos la estructura condicional if else. Utilice tambien el Integer para poder cambiar cualquier valor para hacerlo entero.

--- src/labweek2/LaraSamuelLabControl.java
+++ src/labweek2/LaraSamuelLabControl.java
@@ -99,13 +99,107 @@ public class LaraSamuelLabControl {
             System.out.println("Nota menor: "+notaMenor);
             
         }else if(opcion==3){
-            String fecha;
-            
-            System.out.println(" ");
-            System.out.println("CLASES");
-            System.out.print("Ingrese la fecha actual con el formato (día, DD/MM): ");
-            fecha=lea.next();
-            
+            System.out.print("Ingrese la fecha actual (ejemplo: lunes, 19/10): ");
+            String entrada=lea.next().toLowerCase();
+            if (entrada.matches("^[a-zA-Z]+, \\d{1,2}/\\d{1,2}$")) {
+                int coma = entrada.indexOf(',');
+                int barra = entrada.indexOf('/');
+                String diaSemana = entrada.substring(0, coma);
+                int dia=Integer.parseInt(entrada.substring(coma + 2, barra));
+                int mes=Integer.parseInt(entrada.substring(barra + 1));
+                switch (diaSemana) {
+                case "lunes":
+                    if (mes>12 || dia>31) {
+                    System.out.println("Error: Fecha inválida.");
+                    } else {
+                        System.out.print("¿Se tomaron exámenes? (SI/NO): ");
+                        char respuesta= lea.next().charAt(0);
+                        if (respuesta == 'S' || respuesta == 's') {
+                            System.out.print("Número de alumnos aprobados: ");
+                            int aprobados = lea.nextInt();
+                            System.out.print("Número de alumnos reprobados: ");
+                            int reprobados = lea.nextInt();
+                            double porcentajeAprobados= (double) aprobados/(aprobados+reprobados)*100;
+                            System.out.println("Porcentaje de aprobados: "+porcentajeAprobados+"%");
+                        } else {
+                            System.out.println("Tuvieron un maravilloso día de clases");
+                        }
+                    }
+                    break;
+                case "martes":
+                    if (mes>12 || dia>31) {
+                        System.out.println("Error: Fecha inválida.");
+                    } else {
+                        System.out.print("¿Se tomaron exámenes? (SI/NO): ");
+                        char respuesta= lea.next().charAt(0);
+                        if (respuesta == 'S' || respuesta == 's') {
+                            System.out.print("Número de alumnos aprobados: ");
+                            int aprobados = lea.nextInt();
+                            System.out.print("Número de alumnos reprobados: ");
+                            int reprobados = lea.nextInt();
+                            double porcentajeAprobados= (double) aprobados/(aprobados+reprobados)*100;
+                            System.out.println("Porcentaje de aprobados: "+porcentajeAprobados+"%");
+                        } else {
+                            System.out.println("Tuvieron un maravilloso día de clases");
+                        }
+                    }
+                    break;
+                case "miércoles":
+                    if (mes>12 || dia>31) {
+                        System.out.println("Error: Fecha inválida.");
+                    } else {
+                            //miercoles
+                             System.out.print("¿Se tomaron exámenes? (SI/NO): ");
+                             char respuesta= lea.next().charAt(0);
+                        if (respuesta == 'S' || respuesta == 's') {
+                            System.out.print("Número de alumnos aprobados: ");
+                            int aprobados = lea.nextInt();
+                            System.out.print("Número de alumnos reprobados: ");
+                            int reprobados = lea.nextInt();
+                            double porcentajeAprobados= (double) aprobados/(aprobados+reprobados)*100;
+                            System.out.println("Porcentaje de aprobados: "+porcentajeAprobados+"%");
+                        } else {
+                            System.out.println("Tuvieron un maravilloso día de clases");
+                        }
+                    }
+                    break;
+                case "jueves":
+                    if (mes>12 || dia>31) {
+                        System.out.println("Error: Fecha inválida.");
+                    } else {
+                        //jueves
+                        System.out.print("Porcentaje de asistencia a clase: ");
+                        double asistencia=lea.nextDouble();
+                        if (asistencia>50) {
+                            System.out.println("Asistió la mayoría. \nUn porcentaje de"+asistencia+"%");
+                        } else {
+                            System.out.println("No asistió la mayoría. \nUn porcentaje de "+asistencia+"%");
+                        } 
+                    }
+                    break;
+                case "viernes":
+                    if ((mes==1 || mes==7) && dia==1){
+                        System.out.print("Comienzo de un Nuevo Ciclo\n");
+                        System.out.print("Ingrese cantidad de Alumnos del Nuevo Ciclo: ");
+                        int cantidadAlumnos=lea.nextInt();
+                        System.out.print("Ingrese el Precio por cada Alumno: $. ");
+                        double precioAlumnos=lea.nextDouble(); 
+                        double totalIngreso=0;
+                        totalIngreso=cantidadAlumnos*precioAlumnos;
+                        System.out.println("El Ingreso Total fue de: $."+totalIngreso);
+                    } else {
+                        System.out.println("No se inicia el comienzo de ciclo"); 
+                    }
+                    break;
+                    default:
+                    System.out.println("Error: Día de la semana no válido.");
+                    break;
+                    }
+            } else {
+            System.out.println("Error: Formato de fecha incorrecto.");
+            }
+            System.out.println("\n");
+            break;
         }else{
             System.out.println("Opcion no valida."); 
         }
